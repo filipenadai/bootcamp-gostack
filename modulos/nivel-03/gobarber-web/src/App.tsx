52c49034a4d3c7e05930a925056c92f4ef1b4272
Filipe Nadai <f.nadai05@gmail.com>
2020-11-19 16:00:44 -0300

    create sign base page

--- modulos/nivel-03/gobarber-web/src/App.tsx
+++ modulos/nivel-03/gobarber-web/src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>A</h1>;
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>A</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
