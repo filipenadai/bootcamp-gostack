f6ed9358394078631afaff79ce3e97d248dfef42
Filipe Nadai <f.nadai05@gmail.com>
2020-11-21 10:10:56 -0300

    finish page basic structure

--- modulos/nivel-03/gobarber-web/src/components/Input/styles.ts
+++ modulos/nivel-03/gobarber-web/src/components/Input/styles.ts
@@ -1,5 +1,7 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
@@ -52,5 +54,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background-color: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
