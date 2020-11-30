a84b28dd921e9559b53c2508cfcdf6b472bd4e81
Filipe Nadai <f.nadai05@gmail.com>
2020-11-30 17:51:40 -0300

    tests and fake repositories

---
+++ modules/gobarber/backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,41 @@
+import { v4 } from 'uuid';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import User from '@modules/users/infra/typeorm/entities/User';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+class UsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  public async findById(id: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.id === id);
+
+    return findUser;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.email === email);
+
+    return findUser;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findUserIndex = this.users.findIndex(u => u.id === user.id);
+
+    this.users[findUserIndex] = user;
+
+    return user;
+  }
+
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: v4() }, userData);
+
+    this.users.push(user);
+
+    return user;
+  }
+}
+
+export default UsersRepository;
