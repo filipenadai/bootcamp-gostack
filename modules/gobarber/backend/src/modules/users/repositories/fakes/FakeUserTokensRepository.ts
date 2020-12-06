5afb9321a253378d8f42649d588961c890f3a4d6
Filipe Nadai <f.nadai05@gmail.com>
2020-12-06 12:25:39 -0300

    finish unitary tests

--- modules/gobarber/backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ modules/gobarber/backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -13,9 +13,19 @@ class FakeUserTokensRepository implements IUserTokensRepository {
       id: v4(),
       token: v4(),
       user_id,
+      created_at: Date.now(),
+      updated_at: Date.now(),
     });
 
     this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
 
     return userToken;
   }
