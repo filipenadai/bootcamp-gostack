8729582d201cd4872cfecf92f1033c8c243e3262
Filipe Nadai <f.nadai05@gmail.com>
2020-12-21 10:39:26 -0300

    initializing with mongo

---
+++ modules/gobarber/backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+
+import { container } from 'tsyringe';
+
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+
+export default class AppointmentsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const provider_id = request.user.id;
+    const { year, month, day } = request.body;
+
+    const createAppointment = container.resolve(
+      ListProviderAppointmentsService,
+    );
+
+    const appointments = await createAppointment.execute({
+      provider_id,
+      year,
+      month,
+      day,
+    });
+
+    return response.json(appointments);
+  }
+}
