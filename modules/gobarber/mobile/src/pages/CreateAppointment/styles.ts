8b82af694e0d6188bde7b0737f2385d54dd4fb55
Filipe Nadai <f.nadai05@gmail.com>
2021-01-28 11:11:51 -0300

    list providers in create appointment

--- modules/gobarber/mobile/src/pages/CreateAppointment/styles.ts
+++ modules/gobarber/mobile/src/pages/CreateAppointment/styles.ts
@@ -1,4 +1,14 @@
+import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
+import { Provider } from '.';
+
+interface ProviderContainerProps {
+  selected: boolean;
+}
+
+interface ProviderNameProps {
+  selected: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
@@ -27,4 +37,34 @@ export const UserAvatar = styled.Image`
   height: 56px;
   border-radius: 28px;
   margin-left: auto;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px;
+`;
+
+export const ProvidersListContainer = styled.View`
+  height: 112px;
+`;
+
+export const ProviderContainer = styled.View<ProviderContainerProps>`
+  background: ${props => (props.selected ? '#ff9000' : '#3e3b47')};
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 12px;
+  margin-right: 16px;
+  border-radius: 10px;
+`;
+
+export const ProviderAvatar = styled.Image`
+  height: 32px;
+  width: 32px;
+  border-radius: 16px;
+`;
+
+export const ProviderName = styled.Text<ProviderNameProps>`
+  margin-left: 8px;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+  color: ${props => (props.selected ? '#232129' : '#f4ede8')};
 `;
